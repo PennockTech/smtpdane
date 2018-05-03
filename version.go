815b0e3afbbe867d7924f00e531f9ab3fedaedc3
Phil Pennock <phil@pennock-tech.com>
2018-05-02 21:03:49 -0400

    Version bump to 0.3.0

--- version.go
+++ version.go
@@ -13,7 +13,7 @@ import (
 const programName = "smtpdane"
 
 // may be updated by the linker on the link command-line when compiling
-var VersionString string = "0.2.2"
+var VersionString string = "0.3.0"
 
 // may be updated by the linker on the link command-line when compiling
 var RepoVersionString string = ""
