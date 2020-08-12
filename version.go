2ceab3873460079ac52cb4585964ca4ed1779b4a
Phil Pennock <phil@pennock-tech.com>
2020-08-12 10:30:33 -0400

    Bump version to 0.3.3
    The change for stability of error handling is something I want dependency
    tracking tools to pick up on "sooner", so a preemptive patch release is called
    for.

--- version.go
+++ version.go
@@ -13,7 +13,7 @@ import (
 const programName = "smtpdane"
 
 // may be updated by the linker on the link command-line when compiling
-var VersionString string = "0.3.2"
+var VersionString string = "0.3.3"
 
 // may be updated by the linker on the link command-line when compiling
 var RepoVersionString string = ""
