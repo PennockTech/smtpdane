7251852944232fe78070e0ce94f90c3f7cb6d513
Phil Pennock <phil+git@pennock-tech.com>
2025-09-02 12:57:15 -0400

    admin: updates.sh warn retracted in prior state
    We also need to know if the _prior_ state depended upon modules which have now
    been retracted, so that we can investigate why they were retracted and if that
    in turn requires us to take action.
    
    This should help with supply-chain attacks, somewhat.
    
    Open question as to whether or not we should do the same with zizmor
    before/after the pinact checks.

--- build/updates.sh
+++ build/updates.sh
@@ -2,25 +2,49 @@
 set -eu
 
 repo_root="$(git rev-parse --show-toplevel)"
+progname="${0##*/}"
+stderr() { printf >&2 '%s: %s\n' "$progname" "$*"; }
 
-set -x
+# This roughly matches the output format of `set -x` but lets us interleave
+# more complex functions and be more explicit about what we actually run.
+run() {
+  printf >&2 '+ %s\n' "$*"
+  "$@"
+}
 
-go get -u
-go mod tidy
+set_retracted_golang() {
+  local t
+  t="$(go list -mod=mod -m -retracted -f '{{if .Retracted}}  {{.Path}} is retracted{{end}}' all)"
+  if [ -n "$t" ]; then
+    RETRACTED="$t"
+    return 1
+  fi
+  return 0
+}
+
+
+# This one reports the _PRIOR_ state, which we might be moving away from, and
+# is to let us know if we were using code which had to be retracted, so we can
+# investigate the cause.  Fingers crossed that it's not a supply chain attack.
+stderr "checking if prior state includes retracted dependencies"
+set_retracted_golang || true
+
+stderr "updating Go dependencies"
+run go get -u
+run go mod tidy
 # This one syncs any additional go.sum changes needed:
-go list -mod=mod -m -retracted all > /dev/null
+run go list -mod=mod -m -retracted all > /dev/null
 # and this one will report any retracted dependencies as an error:
-set +x
-t="$(go list -m -retracted -f '{{if .Retracted}}  {{.Path}} is retracted{{end}}' all)"
-if [ -n "$t" ]; then
-  printf >&2 '%s: error: retracted dependencies exist:\n' "${0##*/}"
-  printf >&2 '%s\n' "$t"
+if ! set_retracted_golang; then
+  stderr "error: retracted dependencies exist:"
+  printf >&2 '%s\n' "$RETRACTED"
   exit 1
 fi
 
-set -x
-cd "$repo_root/.github/workflows"
-pinact run -u -- *.y*ml
+stderr "updating GitHub Actions"
+run cd "$repo_root/.github/workflows"
+run pinact run -u -- *.y*ml
+run cd "$repo_root"
 
-cd "$repo_root"
-zizmor --persona pedantic .
+stderr "auditing GitHub Actions"
+run zizmor --persona pedantic .
