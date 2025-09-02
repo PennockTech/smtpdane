5f1471d0f2f368881241cc25ede9f68b86bf9b19
Phil Pennock <phil+git@pennock-tech.com>
2025-09-02 12:46:59 -0400

    admin: have updates.sh error out if retracted deps
    Don't wait for a CI flow to error out on us. If we're running
    `./build/updates.sh` then we are in the right mindset to be investigating and
    fixing.  Now is the time to report issues.

--- build/updates.sh
+++ build/updates.sh
@@ -7,8 +7,18 @@ set -x
 
 go get -u
 go mod tidy
+# This one syncs any additional go.sum changes needed:
 go list -mod=mod -m -retracted all > /dev/null
+# and this one will report any retracted dependencies as an error:
+set +x
+t="$(go list -m -retracted -f '{{if .Retracted}}  {{.Path}} is retracted{{end}}' all)"
+if [ -n "$t" ]; then
+  printf >&2 '%s: error: retracted dependencies exist:\n' "${0##*/}"
+  printf >&2 '%s\n' "$t"
+  exit 1
+fi
 
+set -x
 cd "$repo_root/.github/workflows"
 pinact run -u -- *.y*ml
 
